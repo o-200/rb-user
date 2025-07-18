0b4c45fb6e78fe7977e89cec1a93e04a58cf874e
Alex Abramov <a.abramov@workaround.tech>
2025-07-18 20:23:09 +0300

    add logout, verify, tokens in db

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
+
 from app.schemas import UserCreate, UserLogin, Token
-from app.auth.service import register_user, authenticate_user
+from app.auth.service import register_user, authenticate_user, invalidate_user, verify_token
+from app.auth.utils import get_token
 from app.db import get_db
 
 router = APIRouter(prefix="/auth", tags=["auth"])
@@ -14,3 +16,12 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
 @router.post("/login", response_model=Token)
 def login(credentials: UserLogin, db: Session = Depends(get_db)):
     return authenticate_user(db, credentials)
+
+@router.post("/logout", status_code=status.HTTP_204_NO_CONTENT)
+def logout(token: str = Depends(get_token), db: Session = Depends(get_db)):
+    invalidate_user(db, token)
+    return
+
+@router.get("/verify", status_code=status.HTTP_200_OK)
+def verify(token: str = Depends(get_token), db: Session = Depends(get_db)):
+    return verify_token(db, token)
--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+
+from datetime import datetime
 
 Base = declarative_base()
 
@@ -12,3 +14,19 @@ class User(Base):
     username = Column(String, unique=True, index=True)
 
     hashed_password = Column(String)
+
+    tokens = relationship("Token", back_populates="user")
+
+class Token(Base):
+    __tablename__ = "tokens"
+
+    id = Column(Integer, primary_key=True, index=True)
+    access_token = Column(String, nullable=False, unique=True)
+    refresh_token = Column(String, nullable=False, unique=True)
+    is_valid = Column(Boolean, default=True)
+    token_type = Column(String, default="bearer")
+
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="tokens")
